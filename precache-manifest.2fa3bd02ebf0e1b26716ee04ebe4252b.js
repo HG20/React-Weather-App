20e966d3a57c5456f35d2a75965ec02c69cd16df
Horea <horea.ginsca@gmail.com>
2018-10-26 22:50:56 +0300

    Updates

---
+++ precache-manifest.2fa3bd02ebf0e1b26716ee04ebe4252b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/React-Weather-App/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "1f86f10a848c65ef1c07",
+    "url": "/React-Weather-App/static/css/main.df2c84c6.chunk.css"
+  },
+  {
+    "revision": "53b243b486d5e6f39aebac8dcb5c15a2",
+    "url": "/React-Weather-App/static/media/seasons.53b243b4.jpg"
+  },
+  {
+    "revision": "c6f27fff044e0a2ae409",
+    "url": "/React-Weather-App/static/js/1.c6f27fff.chunk.js"
+  },
+  {
+    "revision": "90affcdec194e114bfca",
+    "url": "/React-Weather-App/static/js/runtime~main.90affcde.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/React-Weather-App/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "1f86f10a848c65ef1c07",
+    "url": "/React-Weather-App/static/js/main.1f86f10a.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/React-Weather-App/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/React-Weather-App/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/React-Weather-App/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "373be77fe818e854d0eda4824daf104f",
+    "url": "/React-Weather-App/static/media/weather.373be77f.jpg"
+  },
+  {
+    "revision": "c6f27fff044e0a2ae409",
+    "url": "/React-Weather-App/static/css/1.360cc52f.chunk.css"
+  },
+  {
+    "revision": "a2498d8dadae515880fb1308a3821eca",
+    "url": "/React-Weather-App/index.html"
+  }
+];
